ab01bbbe7a9ed9044d0b9e9029f330319ad6b8fa
YeeHsun <33437579+YeeHsun@users.noreply.github.com>
2019-10-05 23:27:53 +0800

    Update controller.v

--- hw01/bonus/controller.v
+++ hw01/bonus/controller.v
@@ -1,8 +1,7 @@
 module controller (
   input  [1:0] sw,
- // input  [3:0] btn,
+  input  [3:0] btn,
   output reg control_r_out,control_y_out,control_g_out
-  //output reg first_out
 );
 always@(*)begin
 	case(sw)
@@ -10,24 +9,20 @@ always@(*)begin
 			control_r_out=0;
 			control_y_out=0;
 			control_g_out=0;
-			//first_out=0;
 		end
 		2'd1:begin
-			//first_out=1;
 			if(btn[1]==1)
 				control_y_out=1;
 			else
 			;
 		end
 		2'd2:begin
-			//first_out=1;
 			if(btn[1]==1)
 				control_g_out=1;
 			else
 			;
 		end
 		2'd3:begin
-			//first_out=1;
 			if(btn[1]==1)
 				control_r_out=1;
 			else
