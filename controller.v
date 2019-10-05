ff61b0bc682f47c7e4fc76cadfa7c53b8c0e0b97
YeeHsun <33437579+YeeHsun@users.noreply.github.com>
2019-10-05 15:49:58 +0800

    Update controller.v

--- controller.v
+++ controller.v
@@ -1,7 +1,8 @@
 module controller (
   input  [1:0] sw,
   input  [3:0] btn,
-  output reg control_r_out,control_y+out,control_g_out
+  output reg control_r_out,control_y_out,control_g_out,
+  output reg first_out;
 );
 always@(*)begin
 	case(sw)
@@ -9,20 +10,24 @@ always@(*)begin
 			control_r_out=0;
 			control_y_out=0;
 			control_g_out=0;
+			first_out=0;
 		end
 		2'd1:begin
+			first_out=1;
 			if(btn[1]==1)
 				control_y_out=1;
 			else
 			;
 		end
 		2'd2:begin
+			first_out=1;
 			if(btn[1]==1)
 				control_g_out=1;
 			else
 			;
 		end
 		2'd3:begin
+			first_out=1;
 			if(btn[1]==1)
 				control_r_out=1;
 			else
