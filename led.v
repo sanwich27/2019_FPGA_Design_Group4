e2cf20b6612fe93b1d256113420a66ef63b2f300
E24066357 <47938141+E24066357@users.noreply.github.com>
2019-10-08 15:15:07 +0800

    Update led.v

--- led.v
+++ led.v
@@ -2,7 +2,7 @@ module	RGB_LED(
 	input			clk,		//100MHz
 	input			rst,
 	input	[7:0]		led_in,
-	output	reg [3:0]           led		
+	output	reg [3:0]       led		
 );
 
 wire	[7:0]	next_counter_256;
@@ -11,10 +11,10 @@ reg		[7:0]	counter_256;
 
 assign			next_counter_256 = (counter_256 == 8'd255)? 8'd0 : counter_256 + 8'd1;	//400KHz
 
-assign			led[0] = (counter_256 < R_time_in)? 1'd1 : 1'd0;
-assign			led[1] = (counter_256 < R_time_in)? 1'd1 : 1'd0;
-assign			led[2] = (counter_256 < R_time_in)? 1'd1 : 1'd0;
-assign			led[3] = (counter_256 < R_time_in)? 1'd1 : 1'd0;
+assign			led[0] = (counter_256 < led_in)? 1'd1 : 1'd0;
+assign			led[1] = (counter_256 < led_in)? 1'd1 : 1'd0;
+assign			led[2] = (counter_256 < led_in)? 1'd1 : 1'd0;
+assign			led[3] = (counter_256 < led_in)? 1'd1 : 1'd0;
 
 
 
