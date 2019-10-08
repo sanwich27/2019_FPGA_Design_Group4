9d20f7280e851c967f07c51b0484d03f5a4b96ee
E24066357 <47938141+E24066357@users.noreply.github.com>
2019-10-08 15:14:13 +0800

    Update led_pwm.v

--- led_pwm.v
+++ led_pwm.v
@@ -6,7 +6,7 @@ module  led(
 reg [1:0] cstate;
 reg [1:0] nstate;
 parameter s_reset=2'd0,s_plus=2'd1,s_minus=2'd2;
-  always@(posedge clk_div or posedge rst)begin
+  always@(posedge clk or posedge rst)begin
   	if(rst)
 		cstate<=s_reset;
 	else 
