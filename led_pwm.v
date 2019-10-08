a3bac9bc0e24cf91b5fb01cc97f89db803cab8e1
sanwich27 <33437455+sanwich27@users.noreply.github.com>
2019-10-08 20:04:23 +0800

    Update led_pwm.v

--- led_pwm.v
+++ led_pwm.v
@@ -5,8 +5,10 @@ module  led_pwm(
 );
 reg [1:0] cstate;
 reg [1:0] nstate;
+reg clk_div;
+reg [25:0] cnt;
 parameter s_reset=2'd0,s_plus=2'd1,s_minus=2'd2;
-  always@(posedge clk or posedge rst)begin
+always@(posedge clk_div or posedge rst)begin
   	if(rst)
 		cstate<=s_reset;
 	else 
@@ -21,7 +23,7 @@ always@(*)begin
 		default:nstate=s_reset;
 	endcase
 end
-always@(posedge clk)begin
+	always@(posedge clk_div)begin
 	case(cstate)
 		s_reset:led_out<=8'd0;
 		s_plus:led_out<=led_out+8'd1;
@@ -30,4 +32,19 @@ always@(posedge clk)begin
 	endcase
 end
 endmodule
+always@(posedge clk or posedge rst) begin
+    if (rst) begin
+      cnt <= 26'd0;
+      clk_div <= 'b0;
+    end
+    else begin
 
+      if (cnt == 6250000 - 1) cnt <= 26'd0;
+      else cnt <= cnt + 1;
+
+      if (cnt < 3125000 - 1) clk_div <= 'b0;
+      else clk_div <= 'b1;
+    end
+  end
+
+endmodule
