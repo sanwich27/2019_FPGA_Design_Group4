2ae65f226f6f7fc4cca8989a1138b073d4799be9
E24066357 <47938141+E24066357@users.noreply.github.com>
2019-10-08 15:31:51 +0800

    Update top.v

--- top.v
+++ top.v
@@ -3,18 +3,20 @@ module top(
     input   rst   ,
     output  [3:0] led
     );
+    
     wire led_signal;
+    
   led_pwm a(
       .clk (clk),
       .rst (rst),
       .led_out(led_signal)
 );
-    LED led_0(
-    .clk    (clk_div),
-    .rst    (rst),
-    .sw     (sw),
-    .led    (led)
-    );
+    LED b(
+        .clk (clk),		
+        .rst (rst),
+        .led_in (led_signal),
+        .led	(led)	
+);
     
     
     
