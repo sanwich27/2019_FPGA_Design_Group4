8f0da34f3d9a51813870a1482e85c2bd0c516086
E24066357 <47938141+E24066357@users.noreply.github.com>
2019-10-09 17:59:40 +0800

    Update PWM_Decoder.v

--- hw02_bonus/src/PWM_Decoder.v
+++ hw02_bonus/src/PWM_Decoder.v
@@ -9,7 +9,9 @@ module PWM_Decoder (
   reg [3:0] nstate;
   reg [25:0] cnt;
   reg clk_div;
-	reg [7:0] counter_time;//to show in the same time
+	reg [7:0] counter_time_r;
+	reg [7:0] counter_time_g;
+	reg [7:0] counter_time_b;
   parameter red_plus=4'd0,red_minus=4'd1,orange_plus=4'd2,orange_minus=4'd3,yellow_plus=4'd4,yellow_minus=4'd5,green_plus=4'd6,green_minus=4'd7, blue_plus=4'd8,blue_minus=4'd9,indigo_plus=4'd10,indigo_minus=4'd11,purple_plus=4'd12,purple_minus=4'd13,s_reset=4'd14;
 	always@(posedge clk_div or posedge rst)begin
 	if(rst)
@@ -22,18 +24,18 @@ module PWM_Decoder (
 		s_reset:nstate=red_plus;
 		red_plus:nstate=(R_time_out<8'd254)?red_plus:red_minus;
 		red_minus:nstate=(R_time_out>8'd1)?red_minus:orange_plus;
-		orange_plus:nstate=(R_time_out<8'd254&&G_time_out<8'd59&&counter_time<8'd254)?orange_plus:orange_minus;
-		orange_minus:nstate=(R_time_out>8'd1&&G_time_out>8'd1&&counter_time>8'd1)?orange_minus:yellow_plus;
+		orange_plus:nstate=(R_time_out<8'd254&&G_time_out<8'd59)?orange_plus:orange_minus;
+		orange_minus:nstate=(R_time_out>8'd1&&G_time_out>8'd1)?orange_minus:yellow_plus;
 		yellow_plus:nstate=(R_time_out<8'd254&&G_time_out<8'd254)?yellow_plus:yellow_minus;
 		yellow_minus:nstate=(R_time_out>8'd1&&G_time_out>8'd1)?yellow_minus:green_plus;
 		green_plus:nstate=(G_time_out<8'd254)?green_plus:green_minus;
 		green_minus:nstate=(G_time_out>8'd1)?green_minus:blue_plus;
 		blue_plus:nstate=(B_time_out<8'd254)?blue_plus:blue_minus;
 		blue_minus:nstate=(B_time_out>8'd1)?blue_minus:indigo_plus;
-		indigo_plus:nstate=(R_time_out<8'd7&&G_time_out<8'd45&&B_time_out<8'd83&&counter_time<8'd254)?indigo_plus:indigo_minus;
-		indigo_minus:nstate=(R_time_out>8'd1&&G_time_out>8'd1&&B_time_out>8'd1&&counter_time>8'd1)?indigo_minus:purple_plus;
-		purple_plus:nstate=(R_time_out<8'd159&&G_time_out<8'd31&&B_time_out<8'd239&&counter_time<8'd254)?purple_plus:purple_minus;
-		purple_minus:nstate=(R_time_out>8'd1&&G_time_out>8'd1&&B_time_out>8'd1&&counter_time>8'd1)?purple_minus:red_plus;
+		indigo_plus:nstate=(R_time_out<8'd7&&G_time_out<8'd45&&B_time_out<8'd83)?indigo_plus:indigo_minus;
+		indigo_minus:nstate=(R_time_out>8'd1&&G_time_out>8'd1&&B_time_out>8'd1)?indigo_minus:purple_plus;
+		purple_plus:nstate=(R_time_out<8'd159&&G_time_out<8'd31&&B_time_out<8'd239)?purple_plus:purple_minus;
+		purple_minus:nstate=(R_time_out>8'd1&&G_time_out>8'd1&&B_time_out>8'd1)?purple_minus:red_plus;
 		default:nstate=s_reset;
 	endcase
   end
@@ -43,7 +45,9 @@ module PWM_Decoder (
 			R_time_out<=8'd0;
 			G_time_out<=8'd0;
 			B_time_out<=8'd0;
-			counter_time<=0;
+			counter_r<=0;
+			counter_g<=0;
+			counter_b<=0;
 		end
 		red_plus:begin
 			R_time_out<=R_time_out+8'd1;
@@ -56,15 +60,25 @@ module PWM_Decoder (
 			B_time_out<=8'd0;
 		end
 		orange_plus:begin
-			counter_time<=counter_time+8'd1;
 			R_time_out<=(R_time_out<8'd255)?R_time_out+8'd1:8'd255;
-			G_time_out<=(G_time_out<8'd60)?R_time_out+8'd1:8'd60;
+			if(counter_g<4)begin
+				counter_g<=counter_g+8'd1;
+			end
+			else begin
+				G_time_out<=(G_time_out<8'd60)?R_time_out+8'd1:8'd60;
+				counter_g<=8'd0;
+			end
 			B_time_out<=8'd0;
 		end
 		orange_minus:begin
-			counter_time<=counter_time-8'd1;
 			R_time_out<=(R_time_out>0)?R_time_out-8'd1:8'd0;
-			G_time_out<=(G_time_out>0)?G_time_out-8'd1:8'd0;
+			if(counter_g<4)begin
+				counter_g<=counter_g+8'd1;
+			end
+			else begin
+				G_time_out<=(G_time_out>0)?G_time_out-8'd1:8'd0;
+				counter_g<=8'd0;
+			end
 			B_time_out<=8'd0;
 		end
 		yellow_plus:begin
@@ -98,25 +112,40 @@ module PWM_Decoder (
 			B_time_out<=B_time_out-8'd1;
 		end
 		indigo_plus:begin
-			counter_time<=counter_time+8'd1;
-			R_time_out<=(R_time_out<8'd8)?R_time_out+8'd1:8'd8;
-			G_time_out<=(G_time_out<8'd46)?R_time_out+8'd1:8'd46;
-			B_time_out<=(B_time_out<8'd84)?R_time_out+8'd1:8'd84;
+			if(counter_r<8'd32)begin
+				counter_r<=counter_r+8'd1;
+			end
+			else begin
+				R_time_out<=(R_time_out<8'd8)?R_time_out+8'd1:8'd8;
+				counter_r<=8'd0;
+			end
+			if(counter_g<8'd6)begin
+				counter_g<=counter_g+8'd1;
+			end
+			else begin
+				G_time_out<=(G_time_out<8'd46)?R_time_out+8'd1:8'd46;
+				counter_g<=8'd0;
+			end
+			if(counter_b<8'd3)begin
+				counter_b<=counter_b+8'd1;
+			end
+			else begin
+				B_time_out<=(B_time_out<8'd84)?R_time_out+8'd1:8'd84;
+				counter_b<=8'd0;
+			end
+			
 		end
 		indigo_minus:begin
-			counter_time<=counter_time-8'd1;
 			R_time_out<=(R_time_out>0)?R_time_out-8'd1:8'd0;
 			G_time_out<=(G_time_out>0)?G_time_out-8'd1:8'd0;
 			B_time_out<=(B_time_out>0)?B_time_out-8'd1:8'd0;
 		end
 		purple_plus:begin
-			counter_time<=counter_time+8'd1;
 			R_time_out<=(R_time_out<8'd160)?R_time_out+8'd1:8'd160;
 			G_time_out<=(G_time_out<8'd32)?R_time_out+8'd1:8'd32;
 			B_time_out<=(B_time_out<8'd240)?R_time_out+8'd1:8'd240;
 		end
 		purple_minus:begin
-			counter_time<=counter_time-8'd1;
 			R_time_out<=(R_time_out>0)?R_time_out-8'd1:8'd0;
 			G_time_out<=(G_time_out>0)?G_time_out-8'd1:8'd0;
 			B_time_out<=(B_time_out>0)?B_time_out-8'd1:8'd0;
