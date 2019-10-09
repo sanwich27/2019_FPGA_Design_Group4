b2fd6a86faa160da1a4be344ab6e4c38ab3b2d4b
E24066357 <47938141+E24066357@users.noreply.github.com>
2019-10-09 16:22:50 +0800

    Update 陳哲均請訂正.v

--- 陳哲均請訂正.v
+++ 陳哲均請訂正.v
@@ -1,58 +1,89 @@
-module	RGB_LED(
-	input			clk,		//100MHz
-	input			rst,
-	input	[7:0]	R_time_in,
-	input	[7:0]	G_time_in,
-	input	[7:0]	B_time_in,
-	output			R_out,
-	output			G_out,
-	output			B_out
-	reg [3:0] nstate;
+module PWM_Decoder (
+ 
+  output reg [7:0] R_time_out,
+  reg [3:0] nstate;
+  reg [3:0] cstate;
+  parameter add=2'd0,sub=2'd1,
 );
 
-wire	[7:0]	next_counter_256;
+  always@(posedge clk or posedge rst)
+    begin
+      if(rst)
+        R_time_out<=0;
+        nstate<=add;
+        else
+          cstate<=nstate;
+    end
+  
+  
+  
+ always@(posedge clk or posedge rst)
+    begin
+      case(cstate)
+        add:
+          begin
+            R_time_out=(R_time_out==8'd254)?8'd255:R_time_out+1;
+          end
+        sub:
+          begin
+            R_time_out = (R_time_out == 8'd1)? 8'd0 : R_time_out - 8'd1;
+          end
+        
+       
+    end 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+always @ ( * ) begin
+  //////////
+  case(cstate)
+   add:
+      begin
+      
 
-reg		[7:0]	counter_256;
-parameter add=2'd0,sub=2'd1,
-	//400KHz
+    if(R_time_out==8'd255)
+    begin
+    nstate=sub;
+    end
+        
+    else 
+    begin
+    nstate=add; 
+    end 
+      
+      
+  end
+    //////////
+    sub:
+    begin
+   
+      if(R_time_out==8'd0)
+    begin
+    nstate=add;
+    end
+        
+    else 
+    begin
+    nstate=sub; 
+    end  
+      
+      
+      
+    end
+    
+    
+  endcase
+    
+    
+    
+  end//*號給狀態
 
-assign			R_out = (counter_256 < R_time_in)? 1'd1 : 1'd0;
-assign			G_out = (counter_256 < G_time_in)? 1'd1 : 1'd0;
-assign			B_out = (counter_256 < B_time_in)? 1'd1 : 1'd0;
-counter_256	<= 8'd0;
-nstate=add;
-always @(posedge clk or posedge rst)
-begin
-case(nstate)
-	
-	add:
-	begin
-	next_counter_256 = (counter_256 == 8'd254)? 8'd255 : counter_256 + 8'd1;
-	counter_256	<= next_counter_256;
-	if(next_counter_256==255)
-	begin
-	nstate=sub;
-	end
-	else 
-	begin
-	nstate=add; 
-	end
-	end
-	sub:
-	begin
-	next_counter_256 = (counter_256 == 8'd1)? 8'd0 : counter_256 - 8'd1;
-	counter_256	<= next_counter_256;
-	if(next_counter_256==0)
-		begin
-	nstate=add;
-		end
-	else 
-		begin
-		nstate=sub;
-	        end
-	end
-	
-	
-end
-
-endmodule
+endmodule // Decoder
