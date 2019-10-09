1f03220ff7a57da0d8da1b609c97d37353a0d667
YeeHsun <33437579+YeeHsun@users.noreply.github.com>
2019-10-09 22:31:00 +0800

    Update led_pwm.v

--- hw02_2/src/led_pwm.v
+++ hw02_2/src/led_pwm.v
@@ -5,8 +5,8 @@ module  led_pwm(
 );
 reg [1:0] cstate;
 reg [1:0] nstate;
-reg clk_div;
-reg [25:0] cnt;
+reg clk_div;//clock divider
+reg [25:0] cnt;//counter for clock divider
 parameter s_reset=2'd0,s_plus=2'd1,s_minus=2'd2;
 always@(posedge clk_div or posedge rst)begin
   	if(rst)
@@ -14,16 +14,18 @@ always@(posedge clk_div or posedge rst)begin
 	else 
 		cstate<=nstate;
 		
-  end
+end
 always@(*)begin
 	case(cstate)
 		s_reset:nstate=s_plus;
-		s_plus:nstate=(led_out>8'd253)?s_minus:s_plus;
-		s_minus:nstate=(led_out<8'd2)?s_plus:s_minus;
+		s_plus:nstate=(led_out>8'd253)?s_minus:s_plus;//keep adding until led_out==8'd254 
+		                                              //not equal to 8'd255 since the boundry condition
+		s_minus:nstate=(led_out<8'd2)?s_plus:s_minus;//keep decreasing until led_out==8'd1
+		 					     //not equal to 8'd0 since the boundry condition
 		default:nstate=s_reset;
 	endcase
 end
-	always@(posedge clk_div)begin
+always@(posedge clk_div)begin
 	case(cstate)
 		s_reset:led_out<=8'd0;
 		s_plus:led_out<=led_out+8'd1;
@@ -31,20 +33,19 @@ end
 		default:;
 	endcase
 end
-	always@(posedge clk or posedge rst) begin
-    if (rst) begin
-      cnt <= 26'd0;
-      clk_div <= 'b0;
-    end
-    else begin
+always@(posedge clk or posedge rst) begin//clock divider
+	if (rst) begin
+      		cnt <= 26'd0;
+     		clk_div <= 'b0;
+  	end
+    	else begin
+      		if (cnt == 625000 - 1) cnt <= 26'd0;
+     		else cnt <= cnt + 1;
 
-      if (cnt == 625000 - 1) cnt <= 26'd0;
-      else cnt <= cnt + 1;
-
-      if (cnt < 312500 - 1) clk_div <= 'b0;
-      else clk_div <= 'b1;
-    end
-  end
+      		if (cnt < 312500 - 1) clk_div <= 'b0;
+      		else clk_div <= 'b1;
+    	end
+end
 
 endmodule
 
