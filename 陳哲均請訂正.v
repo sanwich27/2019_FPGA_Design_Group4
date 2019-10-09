51ef7985b5c1ac44fde03e656e3da671dd430cfe
E24066357 <47938141+E24066357@users.noreply.github.com>
2019-10-09 18:55:54 +0800

    Update 陳哲均請訂正.v

--- 陳哲均請訂正.v
+++ 陳哲均請訂正.v
@@ -72,5 +72,22 @@ always @ ( * ) begin
     
     
   end//*號給狀態
+  
+  
+  
+  
+  always@(posedge clk or posedge rst) begin
+    if (rst) begin
+      cnt <= 26'd0;
+      clk_div <= 'b0;
+    end
+    else begin
 
+      if (cnt == 6250000 - 1) cnt <= 26'd0;
+      else cnt <= cnt + 1;
+
+      if (cnt < 3125000 - 1) clk_div <= 'b0;
+      else clk_div <= 'b1;
+    end
+  end
 endmodule // Decoder
