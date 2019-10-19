9749822243562ad3e35c4b7a9319b02b13328d80
sanwich27 <33437455+sanwich27@users.noreply.github.com>
2019-10-20 01:13:44 +0800

    Update RGB.c

--- hw03/Program1/src/RGB.c
+++ hw03/Program1/src/RGB.c
@@ -1,18 +1,3 @@
-/*
- * RGB.c
- *
- *  Created on: 2019年10月17日
- *      Author: user
- */
-
-
-/*
- * hw03RGB.c
- *
- *  Created on: 2019年10月16日
- *      Author: user
- */
-
 
 #include "xparameters.h"
 #include "xgpio.h"
@@ -45,121 +30,128 @@ int main() {
 	XGpio_SetDataDirection(&G_Gpio, 1, 0x00);
 	XGpio_SetDataDirection(&B_Gpio, 1, 0x00);
 
-	while (1) {
+	while (1) { //keep doing
 
 			xil_printf("change_color = %d\r\n", change_color);
 			switch(change_color){
 			case 0:
-				for (int Delay = 0; Delay < LED_DELAY; Delay++){
-				for(int i=0;i<255;i++) //red
+				for (int Delay = 0; Delay < LED_DELAY; Delay++)//just like a while loop, to keep the light continuous
 				{
-					if(i<255)	R_out=0x01;
-					else		R_out=0x00;
-					G_out=0x00;
-					B_out=0x00;
-					XGpio_DiscreteWrite(&R_Gpio, 1, R_out);
-					XGpio_DiscreteWrite(&G_Gpio, 1, G_out);
-					XGpio_DiscreteWrite(&B_Gpio, 1, B_out);
-				}
+					for(int i=0;i<255;i++) //red
+					{
+						if(i<255)	R_out=0x01; 
+						else		R_out=0x00;
+						G_out=0x00;
+						B_out=0x00;
+						XGpio_DiscreteWrite(&R_Gpio, 1, R_out); //write R_out value into &R_Gpio
+						XGpio_DiscreteWrite(&G_Gpio, 1, G_out);
+						XGpio_DiscreteWrite(&B_Gpio, 1, B_out);
+					}
 				}
 				break;
 			case 1:
-				for (int Delay = 0; Delay < LED_DELAY; Delay++){
-							for(int i=0;i<255;i++) //orange
-							{
-								if(i<255)	R_out=0x01;
-								else		R_out=0x00;
-								if(i<30)	G_out=0x01;
-								else		G_out=0x00;
-								B_out=0x00;
-								XGpio_DiscreteWrite(&R_Gpio, 1, R_out);
-								XGpio_DiscreteWrite(&G_Gpio, 1, G_out);
-								XGpio_DiscreteWrite(&B_Gpio, 1, B_out);
-							}
+				for (int Delay = 0; Delay < LED_DELAY; Delay++) 
+				{
+					for(int i=0;i<255;i++) //orange
+					{
+						if(i<255)	R_out=0x01;
+						else		R_out=0x00;
+						if(i<30)	G_out=0x01;
+						else		G_out=0x00;
+						B_out=0x00;
+						XGpio_DiscreteWrite(&R_Gpio, 1, R_out);
+						XGpio_DiscreteWrite(&G_Gpio, 1, G_out);
+						XGpio_DiscreteWrite(&B_Gpio, 1, B_out);
+					}
 				}
 				break;
 			case 2:
-				for (int Delay = 0; Delay < LED_DELAY; Delay++){
-							for(int i=0;i<255;i++) //yellow
-							{
-								if(i<255)	R_out=0x01;
-								else		R_out=0x00;
-								if(i<255)	G_out=0x01;
-								else		G_out=0x00;
-								B_out=0x00;
-								XGpio_DiscreteWrite(&R_Gpio, 1, R_out);
-								XGpio_DiscreteWrite(&G_Gpio, 1, G_out);
-								XGpio_DiscreteWrite(&B_Gpio, 1, B_out);
-							}
+				for (int Delay = 0; Delay < LED_DELAY; Delay++)
+				{
+					for(int i=0;i<255;i++) //yellow
+					{
+						if(i<255)	R_out=0x01;
+						else		R_out=0x00;
+						if(i<255)	G_out=0x01;
+						else		G_out=0x00;
+						B_out=0x00;
+						XGpio_DiscreteWrite(&R_Gpio, 1, R_out);
+						XGpio_DiscreteWrite(&G_Gpio, 1, G_out);
+						XGpio_DiscreteWrite(&B_Gpio, 1, B_out);
+					}
 				}
 				break;
 			case 3:
-				for (int Delay = 0; Delay < LED_DELAY; Delay++){
-							for(int i=0;i<255;i++) //green
-							{
-								R_out=0x00;
-								if(i<255)	G_out=0x01;
-								else		G_out=0x00;
-								B_out=0x00;
-								XGpio_DiscreteWrite(&R_Gpio, 1, R_out);
-								XGpio_DiscreteWrite(&G_Gpio, 1, G_out);
-								XGpio_DiscreteWrite(&B_Gpio, 1, B_out);
-							}
+				for (int Delay = 0; Delay < LED_DELAY; Delay++)
+				{
+					for(int i=0;i<255;i++) //green
+					{
+						R_out=0x00;
+						if(i<255)	G_out=0x01;
+						else		G_out=0x00;
+						B_out=0x00;
+						XGpio_DiscreteWrite(&R_Gpio, 1, R_out);
+						XGpio_DiscreteWrite(&G_Gpio, 1, G_out);
+						XGpio_DiscreteWrite(&B_Gpio, 1, B_out);
+					}
 				}
 				break;
 
 			case 4:
-				for (int Delay = 0; Delay < LED_DELAY; Delay++){
-							for(int i=0;i<255;i++) //blue
-							{
-								R_out=0x00;
-								G_out=0x00;
-								if(i<255)	B_out=0x01;
-								else		B_out=0x00;
-								XGpio_DiscreteWrite(&R_Gpio, 1, R_out);
-								XGpio_DiscreteWrite(&G_Gpio, 1, G_out);
-								XGpio_DiscreteWrite(&B_Gpio, 1, B_out);
-							}
+				for (int Delay = 0; Delay < LED_DELAY; Delay++)
+				{
+					for(int i=0;i<255;i++) //blue
+					{
+						R_out=0x00;
+						G_out=0x00;
+						if(i<255)	B_out=0x01;
+						else		B_out=0x00;
+						XGpio_DiscreteWrite(&R_Gpio, 1, R_out);
+						XGpio_DiscreteWrite(&G_Gpio, 1, G_out);
+						XGpio_DiscreteWrite(&B_Gpio, 1, B_out);
+					}
 				}
 				break;
 
 			case 5:
-				for (int Delay = 0; Delay < LED_DELAY; Delay++){
-							for(int i=0;i<255;i++) //indigo
-							{
-								R_out=0x00;
-								if(i<90)	G_out=0x01;
-								else		G_out=0x00;
-								if(i<180)	B_out=0x01;
-								else		B_out=0x00;
-								XGpio_DiscreteWrite(&R_Gpio, 1, R_out);
-								XGpio_DiscreteWrite(&G_Gpio, 1, G_out);
-								XGpio_DiscreteWrite(&B_Gpio, 1, B_out);
-							}
+				for (int Delay = 0; Delay < LED_DELAY; Delay++)
+				{
+					for(int i=0;i<255;i++) //indigo
+					{
+						R_out=0x00;
+						if(i<90)	G_out=0x01;
+						else		G_out=0x00;
+						if(i<180)	B_out=0x01;
+						else		B_out=0x00;
+						XGpio_DiscreteWrite(&R_Gpio, 1, R_out);
+						XGpio_DiscreteWrite(&G_Gpio, 1, G_out);
+						XGpio_DiscreteWrite(&B_Gpio, 1, B_out);
+					}
 				}
-					break;
+				break;
 
 			case 6:
-				for (int Delay = 0; Delay < LED_DELAY; Delay++){
-							for(int i=0;i<255;i++) //purple
-							{
-								if(i<139)	R_out=0x01;
-								else		R_out=0x00;
-								G_out=0x00;
-								if(i<255)	B_out=0x01;
-								else		B_out=0x00;
-								XGpio_DiscreteWrite(&R_Gpio, 1, R_out);
-								XGpio_DiscreteWrite(&G_Gpio, 1, G_out);
-								XGpio_DiscreteWrite(&B_Gpio, 1, B_out);
-							}
+				for (int Delay = 0; Delay < LED_DELAY; Delay++)
+				{
+					for(int i=0;i<255;i++) //purple
+					{
+						if(i<139)	R_out=0x01;
+						else		R_out=0x00;
+						G_out=0x00;
+						if(i<255)	B_out=0x01;
+						else		B_out=0x00;
+						XGpio_DiscreteWrite(&R_Gpio, 1, R_out);
+						XGpio_DiscreteWrite(&G_Gpio, 1, G_out);
+						XGpio_DiscreteWrite(&B_Gpio, 1, B_out);
+					}
 				}
-					break;
+				break;
 			default:
 				xil_printf(" bbbb= %d\r\n", change_color);
 				break;
 			}
 			change_color++;
-			if(change_color==7) {change_color=0;}
+			if(change_color==7) {change_color=0;} //when purple light is overm, continue to red light
 
 		}
 
