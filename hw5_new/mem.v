e639b99f3144b14896c0b9fda658afa2217def27
YeeHsun <33437579+YeeHsun@users.noreply.github.com>
2019-11-27 23:57:25 +0800

    Add files via upload

---
+++ hw5_new/mem.v
@@ -0,0 +1,30 @@
+(* keep_hierarchy = "yes" *) module mem(
+	input clk,
+	input [2:0] cmd_ps,//0:write ps data ; 1:read ps data ; 2:write pl data ; 3:read pl data ; else:idle
+	input [2:0] cmd_pl,//0:write ps data ; 1:read ps data ; 2:write pl data ; 3:read pl data ; else:idle
+	input [31:0] data_in_ps,
+	input [31:0] data_in_pl,
+	input [7:0] address_ps,
+	input [7:0] address_pl,
+	output reg [31:0] data_out
+);
+integer i;
+reg [31:0] mem [0:255]; //32*256
+
+always@(posedge clk)begin 
+    case(cmd_ps)
+		3'd0:mem[address_ps]<=data_in_ps;
+		3'd1:data_out<=mem[address_ps];
+		3'd2:mem[address_pl]<=data_in_pl;
+		3'd3:data_out<=mem[address_pl];
+		default:;
+	endcase
+	case(cmd_pl)
+		3'd0:mem[address_ps]<=data_in_ps;
+		3'd1:data_out<=mem[address_ps];
+		3'd2:mem[address_pl]<=data_in_pl;
+		3'd3:data_out<=mem[address_pl];
+		default:;
+	endcase
+end
+endmodule
---
+++ hw5_new/memory_ctrl.v
@@ -0,0 +1,37 @@
+(* keep_hierarchy = "yes" *)module	memory_ctrl(
+	input 	[31:0]		data_in,//data input from ps
+	input 	[7:0]		address,//address input from ps
+	input   ready, //ready signal from ps
+	input   done, 	//result is ready to output to ps
+	input 	[31:0]      result1,//return the value after processed from memory
+	input 	[31:0]      result2,//return the value after processed from memory
+	input 	[31:0]      result3,//return the value after processed from memory
+	input 	[31:0]      result4,//return the value after processed from memory
+	input 	[31:0]      result5,//return the value after processed from memory
+
+	output 	wire [31:0]		data_ps,
+	output  wire [7:0]		  address_ps,
+	output  wire [31:0]     result_out1, //show the result on ps
+	output  wire [31:0]     result_out2, //show the result on ps
+	output  wire [31:0]     result_out3, //show the result on ps
+	output  wire [31:0]     result_out4, //show the result on ps
+	output  wire [31:0]     result_out5, //show the result on ps
+	output  wire ready_for_pl,
+	output  wire pl_done, //result is ready to output to ps
+	output  wire [2:0] cmd
+);
+
+
+assign data_ps=data_in;
+assign address_ps=address;
+assign ready_for_pl=ready;
+assign result_out1=result1;
+assign result_out2=result2;
+assign result_out3=result3;
+assign result_out4=result4;
+assign result_out5=result5;
+assign cmd=(ready==1'd1)?3'd4:3'd0;
+assign pl_done=done;
+
+
+endmodule
